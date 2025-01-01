7e622b612729f5a2e9d4f2be0c0d4c3d88954562
aLearningLad <mocmanca@gmail.com>
2025-01-01 14:34:45 +0200

    addition of drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grubnerdapp/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,7 +18,7 @@ class _HomePageState extends State<HomePage> {
           // textAlign: TextAlign.center,
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
