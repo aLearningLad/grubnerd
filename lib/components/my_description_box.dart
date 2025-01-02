e1ba9a6a914e2bd8cb5121d6d3f6331e723f2189
aLearningLad <mocmanca@gmail.com>
2025-01-02 23:01:48 +0200

    tab bar added

--- lib/components/my_description_box.dart
+++ lib/components/my_description_box.dart
@@ -5,10 +5,17 @@ class MyDescriptionBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+// text styles
+    var myPrimaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.inversePrimary);
+    var mySecondaryTextStyle =
+        TextStyle(color: Theme.of(context).colorScheme.primary);
+
     return Container(
       decoration: BoxDecoration(
           border: Border.all(color: Theme.of(context).colorScheme.secondary)),
       padding: EdgeInsets.all(20),
+      margin: EdgeInsets.only(left: 25, right: 25, bottom: 25),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -23,8 +30,14 @@ class MyDescriptionBox extends StatelessWidget {
 // delivery time
           Column(
             children: [
-              Text('15-30min'),
-              Text('Delivery time'),
+              Text(
+                '15-30min',
+                style: myPrimaryTextStyle,
+              ),
+              Text(
+                'Delivery time',
+                style: mySecondaryTextStyle,
+              ),
             ],
           )
         ],
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:grubnerdapp/components/my_current_location.dart';
 import 'package:grubnerdapp/components/my_description_box.dart';
 import 'package:grubnerdapp/components/my_drawer.dart';
 import 'package:grubnerdapp/components/my_sliver_app_bar.dart';
+import 'package:grubnerdapp/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,37 +12,54 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+// tab controller
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
       body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) => [
-          MySliverAppBar(
-            title: Text('title here'),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Divider(
-                  indent: 25,
-                  endIndent: 25,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                MySliverAppBar(
+                  title: MyTabBar(tabController: _tabController),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Divider(
+                        indent: 25,
+                        endIndent: 25,
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
 
-                // current location
-                MyCurrentLocation(),
+                      // current location
+                      MyCurrentLocation(),
 
-                // description box
-                MyDescriptionBox()
+                      // description box
+                      MyDescriptionBox()
+                    ],
+                  ),
+                )
               ],
-            ),
-          )
-        ],
-        body: Container(
-          color: Colors.blue,
-        ),
-      ),
+          body: TabBarView(controller: _tabController, children: [
+            Text("First tab for home!"),
+            Text("Second tab with settings!"),
+            Text("Third tab with person!"),
+          ])),
     );
   }
 }
