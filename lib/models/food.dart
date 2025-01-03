18fe605de3cb9b32a6fb140d4e7d1f10cd8253b2
aLearningLad <mocmanca@gmail.com>
2025-01-03 20:40:59 +0200

    food class additions

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -3,10 +3,25 @@ class Food {
   final String description;
   final String imagePath;
   final double price;
+  final FoodCategory category;
+  List<Addon> availableAddons;
 
   Food(
       {required this.name,
       required this.description,
       required this.imagePath,
+      required this.availableAddons,
+      required this.category,
       required this.price});
+}
+
+// food categories
+enum FoodCategory { burgers, salads, sides, desserts, drinks }
+
+// food add-ons
+class Addon {
+  String name;
+  double price;
+
+  Addon({required this.name, required this.price});
 }
