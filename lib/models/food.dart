e6529d94b398dacda57e16b43c9761851a7aa8db
aLearningLad <mocmanca@gmail.com>
2025-01-03 20:28:26 +0200

    adding food class

---
+++ lib/models/food.dart
@@ -0,0 +1,12 @@
+class Food {
+  final String name;
+  final String description;
+  final String imagePath;
+  final double price;
+
+  Food(
+      {required this.name,
+      required this.description,
+      required this.imagePath,
+      required this.price});
+}
